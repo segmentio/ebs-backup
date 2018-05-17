06b69d6a34b6efc42ac594b663939c4dba0d8278
Michael S. Fischer <michael.fischer@segment.com>
2018-05-17 15:31:43 -0700

    Use format instead of substr for truncation

--- terraform/scheduled_backup/lambda.tf
+++ terraform/scheduled_backup/lambda.tf
@@ -9,12 +9,11 @@ data "aws_ssm_parameter" "lambda_s3_key" {
 }
 
 locals {
-  function_name           = "ebs-backup-${var.volume_name}-${replace(join("-", var.device_names), "/\\/dev\\//", "")}"
-  function_name_truncated = "${substr(local.function_name, 0, min(length(local.function_name), 64))}"
+  function_name = "ebs-backup-${var.volume_name}-${replace(join("-", var.device_names), "/\\/dev\\//", "")}"
 }
 
 resource "aws_lambda_function" "ebs_backup" {
-  function_name = "${local.function_name_truncated}"
+  function_name = "${format("%.64s", local.function_name)}"
   handler       = "ebs-backup-lambda"
   role          = "${aws_iam_role.ebs_backup.arn}"
   s3_bucket     = "${coalesce(var.lambda_s3_bucket, join("", data.aws_ssm_parameter.lambda_s3_bucket.*.value))}"
