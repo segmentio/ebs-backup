804d1878f7886405526613c086686668a23ce0f1
pablo-vidal <pablo.vidal@segment.com>
2021-05-12 09:52:44 -0700

    Upgrade module to terraform 12
    This module is only used by dedupe. Since we are migrating to terraform 0.12, we don't need to support 0.11

--- terraform/scheduled_backup/input.tf
+++ terraform/scheduled_backup/input.tf
@@ -1,23 +1,23 @@
 variable "lambda_s3_bucket_ssm_parameter" {
-  type        = "string"
+  type        = string
   default     = "/segment/ebs_backup/lambda_s3_bucket"
   description = "SSM parameter under which name of Lambda S3 bucket will be found"
 }
 
 variable "lambda_s3_key_ssm_parameter" {
-  type        = "string"
+  type        = string
   default     = "/segment/ebs_backup/lambda_s3_key"
   description = "SSM parameter under which name of Lambda S3 key will be found"
 }
 
 variable "lambda_s3_bucket" {
-  type        = "string"
+  type        = string
   description = "S3 bucket containing EBS backup Lambda function.  If specified, will override any value found in the Parameter Store."
   default     = ""
 }
 
 variable "lambda_s3_key" {
-  type        = "string"
+  type        = string
   description = "S3 key pointing to EBS backup Lambda function.  If specified, will override any value found in the Parameter Store."
   default     = ""
 }
@@ -28,13 +28,13 @@ variable "copy_tags" {
 }
 
 variable "frequency" {
-  type        = "string"
+  type        = string
   description = "Frequency at which backup is run (see https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html#RateExpressions for legal values)"
   default     = "2 hours"
 }
 
 variable "timeout" {
-  type        = "string"
+  type        = string
   description = "Maximum runtime for backup Lambda function, in seconds"
   default     = 300
 }
@@ -45,12 +45,12 @@ variable "snapshot_limit" {
 }
 
 variable "volume_name" {
-  type        = "string"
+  type        = string
   description = "Value of `Name` tag on EBS volumes to match"
 }
 
 variable "device_names" {
-  type        = "list"
+  type        = list(string)
   description = "List of device attachment names to match (e.g. `/dev/xvdf`)"
 }
 
@@ -58,3 +58,4 @@ variable "enable_event_rule" {
   default     = true
   description = "Enable event rule (not normally disabled)"
 }
+
